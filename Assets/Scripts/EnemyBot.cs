ad62ea444ad038fe11d940ff12654ad0e25f7807
klimater <serafimspiridonov6@gmail.com>
2025-11-15 12:59:45 +1000

    Управление игрока
    Реализовано управление игрока через "W" и "S"

---
+++ Assets/Scripts/EnemyBot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+   public Transform ball;
+   public Rigidbody2D enemyPaddle;
+   public float speed = 2;
+
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Rigidbody2D rb2d;
+    public float moveSpeed = 2f;
+
+
+    private void Update()
+    {
+        float movement = ProcessInput();
+        Move(movement);
+    }
+
+    private float ProcessInput()
+    {
+        float movement = Input.GetAxis("MovePlayer");
+        return movement;
+    }
     
+
+    private void Move(float movement)
+    {
+
+        Vector2 velo = rb2d.velocity;
+        velo.y = moveSpeed * movement;
+        rb2d.velocity = velo;
+    }
 }
